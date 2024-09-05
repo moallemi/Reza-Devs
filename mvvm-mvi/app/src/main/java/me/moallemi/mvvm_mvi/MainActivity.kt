caed8e6a4d4082712f650755212a9dbf9bbac161
Reza Moallemi <rm.allmails@gmail.com>
2024-09-04 20:36:23 -0700

    Fix styling and theme

--- mvvm-mvi/app/src/main/java/me/moallemi/mvvm_mvi/MainActivity.kt
+++ mvvm-mvi/app/src/main/java/me/moallemi/mvvm_mvi/MainActivity.kt
@@ -1,47 +1,23 @@
 package me.moallemi.mvvm_mvi
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import me.moallemi.mvvm_mvi.ui.theme.MVVMMVITheme
+import androidx.appcompat.app.AppCompatActivity
+import me.moallemi.mvvm_mvi.feature.mvi.MviFragment
+import me.moallemi.mvvm_mvi.feature.mvvm.MvvmFragment
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
-    setContent {
-      MVVMMVITheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-          Greeting(
-            name = "Android",
-            modifier = Modifier.padding(innerPadding)
-          )
-        }
-      }
+
+    setContentView(R.layout.activity_main)
+
+    if (savedInstanceState == null) {
+      supportFragmentManager.beginTransaction()
+        .replace(R.id.fragment_container_1, MvvmFragment.newInstance())
+        .replace(R.id.fragment_container_2, MviFragment.newInstance())
+        .commitAllowingStateLoss()
     }
-  }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-  MVVMMVITheme {
-    Greeting("Android")
   }
 }
